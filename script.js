d019ea105a59406132864e70dcf61ae5db0e18b8
Aror Emmanuel <emmaaror@gmail.com>
2021-03-13 22:45:14 +0100

    second commit still didnt work

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.getElementById("buy_button").addEventListener(click, congratulationMessage);
+
 function congratulationMessage() {
-    windows.alert("Congratulations you just bought new shoes")
+    window.alert ("Congratulations you just bought new shoes");
 }
