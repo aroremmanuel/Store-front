218e3871091816f52b4b86c5b913efbe8e9435cc
Aror Emmanuel <emmaaror@gmail.com>
2021-03-15 17:02:02 +0100

    Added checkout and stuff

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 document.getElementById("checkout_button").addEventListener("click", congratulationMessage);
 
+document.getElementById("buy_button").addEventListener("click", addToCart);
+
+document.getElementById("buy_button2").addEventListener("click", addToCart);
+
+document.getElementById("buy_button").addEventListener("click", rewritePriceAirforce);
+
+document.getElementById("buy_button2").addEventListener("click", rewritePriceAirmax);
+
+function rewritePriceAirmax() {
+    document.getElementById("pair").innerHTML = "NIke Airmax V5";
+    document.getElementById("pricetag").innerHTML = "$200";
+}
+
+function rewritePriceAirforce() {
+    document.getElementById("pair").innerHTML = "NIke Airforce 1";
+    document.getElementById("pricetag").innerHTML = "$200";
+}
+
+function addToCart() {
+    alert ("Added your new Nikes to the cart!!!");
+}
 function congratulationMessage() {
-    window.alert ("Congratulations you just bought new shoes");
+    alert ("Congratulations you just bought new shoes");
 }
